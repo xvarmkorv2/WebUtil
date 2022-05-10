8b8d832fe6df30e8c335c8ee10127fedaf23e630
xvarmkorv2 <49081442+xvarmkorv2@users.noreply.github.com>
2022-05-10 08:05:35 -0400

    slight change to benedict

--- js/benedict.browser.js
+++ js/benedict.browser.js
@@ -2,19 +2,19 @@
 const reason = document.querySelector('#reason');
 const target = document.querySelector('#name');
 const container = document.querySelector('#that-guy');
+const reasons = [
+	"with way too much free time",
+	"because someone had to",
+	"with only minor shame",
+	"out of a dangerously high level of boredom",
+	"but it probably shouldn't have been",
+	"but can you pretend it wasn't?",
+	"but don't tell anyone",
+];
 function roll() {
-	const reasons = [
-		"with way too much free time",
-		"because someone had to",
-		"with only minor shame",
-		"out of a dangerously high level of boredom",
-		"but it probably shouldn't have been",
-		"but can you pretend it wasn't?",
-		"but don't tell anyone",
-	];
 	const name = BENEDICT.name;
 	let fading = true;
-	if (!reason.textContent.trim() || Math.random() < 0.05) {
+	if (!reason.textContent.trim() || Math.random() < 0.075) {
 		const last = reason.textContent.trim();
 		let next;
 		do {
